713640c3142d7c6f0ccb317fb677a13bccf48f67
Seyali-ahm <seyyedaliahmadi9@gmail.com>
2024-12-18 21:16:51 +0100

    edited structure

--- template.py
+++ template.py
@@ -8,7 +8,7 @@ logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 project_name = "text_Summarizer"
 
 list_of_files = [
-    '.github/workflows/.gitkeep',
+    ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
@@ -26,7 +26,7 @@ list_of_files = [
     "Dockerfile",
     "requirements.txt",
     "setup.py",
-    "research/trials.ipynb",
+    "research/trials.ipynb"
 
 
 ]
@@ -37,10 +37,10 @@ for filepath in list_of_files:
     if filedir != "":
         os.makedirs(filedir,exist_ok=True)
         logging.info(f"creating directorty:{filedir} for the file in {filename}")
-        if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
-            with open(filepath, 'w') as f:
-                pass
-                logging.info(f"creating empty file:{filepath}")
-        else:
-            logging.info(f"{filename} already exists")
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath, 'w') as f:
+            pass
+            logging.info(f"creating empty file:{filepath}")
+    else:
+        logging.info(f"{filename} already exists")
 
